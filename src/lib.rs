c422e2586b348fc0bb3362a639e71448bc75a39c
FrogBomb <TLBlanchet@gmail.com>
2017-06-25 20:02:12 -0600

    A few more tests, still need doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,29 @@ pub mod continued_fractions;
 mod tests {
     use continued_fractions::{NormalizedContinuedFraction, ContinuedFraction};
     #[test]
-    fn it_works() {
-    }
-    #[test]
-    fn somethingEasy(){
+    fn something_easy() {
         let test_cf = NormalizedContinuedFraction::new(55, 34);
         assert_eq!(55.0/34.0, test_cf.value());
-        // assert_eq!((55, 34), test_cf.as_rational());
+        assert_eq!((55, 34), test_cf.as_rational());
+        assert_eq!((34, 21), test_cf.fractional_part().inverted().unwrap().as_rational());
+    }
+    #[test]
+    fn bigger_test() {
+        use self::NormalizedContinuedFraction::*;
+        let mut test_cf = NormalizedContinuedFraction::new(1134903170, 701408733);
+        loop {
+            let value = test_cf.value();
+            test_cf = match test_cf {
+                Whole(x) => {
+                    assert_eq!(x, 2, "{}", value);
+                    break
+                },
+                Continuation(x, f) => {
+                    assert_eq!(x, 1, "{}", value);
+                    *f
+                },
+            }
+        }
 
     }
 }
